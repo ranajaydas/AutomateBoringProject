32334c487969d10100ff42f4dc38800f5e379bc1
ranajaydas <ranajay@outlook.com>
2019-08-28 14:16:08 +0300

    Updated xl_update_produce program to add cell formatting

--- xl_update_produce.py
+++ xl_update_produce.py
@@ -1,6 +1,7 @@
 """ Program to update the prices in an excel file, xl_produceSales.xlsx """
 
 import openpyxl
+from openpyxl.styles import Font
 
 print('Opening workbook...')
 wb = openpyxl.load_workbook('xl_produceSales.xlsx')
@@ -11,10 +12,23 @@ price_updates = {'Garlic': 3.77,
                  'Celery': 1.77,
                  'Lemon': 1.42}
 
+# Font style to set for updated rows
+font_style = Font(sz=12,
+                  b=True,
+                  i=True,
+                  color='00FF0000')
+
+# Iterate through all the rows of the excel file
 for row in range(2, sheet.max_row+1):
     produce_name = sheet.cell(row=row, column=1).value
     if produce_name in price_updates:
         sheet.cell(row=row, column=2).value = price_updates[produce_name]
+
+        # Update the style of the changed row
+        sheet.cell(row=row, column=1).font = font_style
+
+# Freeze panes at cell A2
+sheet.freeze_panes = 'A2'
 
 print('Saving new workbook with updated prices...')
 wb.save('xl_produceSales_updated.xlsx')
