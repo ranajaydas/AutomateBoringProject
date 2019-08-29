c2b45e1675941f7a24293ed0e9e2e21edfae0e26
ranajaydas <ranajay@outlook.com>
2019-08-29 10:33:55 +0300

    Updated the program name in docstring

--- web_mapit.py
+++ web_mapit.py
@@ -1,5 +1,5 @@
 #! python3
-# mapIt.py - Launches a map in the browser using an address from the
+# web_mapit.py - Launches a map in the browser using an address from the
 # command line or clipboard.
 import webbrowser
 import sys
