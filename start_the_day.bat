964e9b26d618f1fd059e2931f53df555e61d9a4c
ranajaydas <ranajay@outlook.com>
2019-09-01 21:02:24 +0300

    Added a simple program to start all my commonly used programs

---
+++ start_the_day.bat
@@ -0,0 +1,2 @@
+@py.exe C:\Users\Ranaj\Documents\Dropbox\Learning\Python\Automa~1\AutomateBoringProject\start_the_day.py %*
+start "" "C:\Program Files\PyCharm Community Edition 2019.1.1\bin\pycharm64.exe
---
+++ start_the_day.py
@@ -0,0 +1,33 @@
+""" A Simple Program to Open Relevant Programs at the start of the day """
+
+import os
+import subprocess
+import webbrowser
+
+pdf_path = '../automate-the-boring-stuff-with-python-2015-.pdf'
+music_url = 'https://youtu.be/y9eFk8TuV9k'
+start_at_1 = '?t=1'
+
+
+# Open the Automate Boring Stuff pdf
+proc1 = subprocess.Popen(['start', pdf_path], shell=True)
+print('Opened pdf...')
+
+
+# Open git
+proc2 = subprocess.Popen(r'C:\Program Files\Git\git-bash.exe')
+print('Opened git...')
+
+
+# Open the current working directory
+os.startfile(os.getcwd())
+print('Opened Current Working Directory...')
+
+
+# Open the Web Links
+if 'youtu' in music_url:
+    music_url += start_at_1
+
+webbrowser.open(music_url, 1)
+webbrowser.open('https://github.com/ranajaydas/AutomateBoringProject', 2)
+print('Opened all urls...')
