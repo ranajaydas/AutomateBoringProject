baf4ebe763c5c0bd05d0c5110bd8ee64b3f9cdf9
ranajaydas <ranajay@outlook.com>
2019-09-03 15:36:52 +0300

    Modified the daily startup routine

--- start_the_day.py
+++ start_the_day.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import webbrowser
+import time
 
 pdf_path = '../automate-the-boring-stuff-with-python-2015-.pdf'
 music_url = 'https://youtu.be/y9eFk8TuV9k'
@@ -15,7 +16,6 @@ music_source = 'spotify'
 # Open the Automate Boring Stuff pdf
 proc1 = subprocess.Popen(['start', pdf_path], shell=True)
 print('Opened pdf...')
-
 
 # Open git
 proc2 = subprocess.Popen(r'C:\Program Files\Git\git-bash.exe')
@@ -35,7 +35,6 @@ if music_source is 'youtube':
 
 elif music_source is 'spotify':
     proc3 = subprocess.Popen(r'C:\Users\Ranaj\AppData\Roaming\Spotify\Spotify.exe')
-
 
 # Open the Web Links
 webbrowser.open('https://github.com/ranajaydas/AutomateBoringProject', 2)
