290a745fef29cfde9f44504fa2dcbfcc62e8b951
ranajaydas <ranajay@outlook.com>
2019-09-03 15:00:38 +0300

    Added RGB value detection in mouse

--- kbmouse_mousenow.py
+++ kbmouse_mousenow.py
@@ -7,9 +7,17 @@ import pyautogui
 print('Press Ctrl-C to quit.')
 try:
     while True:
-        # Get and print the mouse coordinates.
+        # Get the mouse coordinates
         x, y = pyautogui.position()
         position = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+
+        # Get the RGB value at this position
+        pixel_colour = pyautogui.screenshot().getpixel((x, y))
+        position += '\tRGB: ' \
+                    + str(pixel_colour[0]).rjust(4) + ',' \
+                    + str(pixel_colour[1]).rjust(4) + ',' \
+                    + str(pixel_colour[2]).rjust(4)
+
         print(position, end='')
 
         # Erase the last value
